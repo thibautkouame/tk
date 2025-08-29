f14665bee358e457d3159dacbb1c5d6cd2681ea8
Admin <admin@192.168.1.17>
2025-08-29 10:26:58 +0000

    correction de cié en Crée

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -392,7 +392,7 @@ export default function Home() {
                             animate={{ opacity: 1, y: 0 }}
                             transition={{ delay: 1.0, duration: 0.6 }}
                         >
-                            <span>Crié des filmes</span>
+                            <span>Crée des filmes</span>
                             <motion.span
                                 className="inline-block mx-1"
                                 animate={{
